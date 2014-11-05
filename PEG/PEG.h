46c5437678017f1982b1411c8fe4802011152369
Kristian Andersen <hello@kristian.co>
2014-11-05 17:40:18 +0100

    Initial Commit

---
+++ PEG/PEG.h
@@ -0,0 +1,19 @@
+//
+//  PEG.h
+//  PEG
+//
+//  Created by Kristian Andersen on 05/11/14.
+//  Copyright (c) 2014 kristian.co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PEG.
+FOUNDATION_EXPORT double PEGVersionNumber;
+
+//! Project version string for PEG.
+FOUNDATION_EXPORT const unsigned char PEGVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PEG/PublicHeader.h>
+
+
---
+++ PEGTests/PEGTests.swift
@@ -0,0 +1,36 @@
+//
+//  PEGTests.swift
+//  PEGTests
+//
+//  Created by Kristian Andersen on 05/11/14.
+//  Copyright (c) 2014 kristian.co. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PEGTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
